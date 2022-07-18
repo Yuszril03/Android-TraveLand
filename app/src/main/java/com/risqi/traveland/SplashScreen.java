1a197cbd3b074b9e641e68ca85f8b2bdd2cb44e3
Yuszril03 <72553056+Yuszril03@users.noreply.github.com>
2022-07-19 00:36:53 +0700

    update splashcreen

--- app/src/main/java/com/risqi/traveland/SplashScreen.java
+++ app/src/main/java/com/risqi/traveland/SplashScreen.java
@@ -1,7 +1,11 @@
 package com.risqi.traveland;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.blogspot.atifsoftwares.animatoolib.Animatoo;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, LoginScreen.class);
+                startActivity(intent);
+                Animatoo.animateFade(SplashScreen.this);
+                finish();
+            }
+        }, 1000);
+
     }
 }
