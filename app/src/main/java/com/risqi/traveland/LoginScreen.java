fe11849cdb2ec9f739058bc43db449721f1b1232
Yuszril03 <72553056+Yuszril03@users.noreply.github.com>
2022-07-19 04:21:33 +0700

    update login

--- app/src/main/java/com/risqi/traveland/LoginScreen.java
+++ app/src/main/java/com/risqi/traveland/LoginScreen.java
@@ -2,13 +2,34 @@ package com.risqi.traveland;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 
 public class LoginScreen extends AppCompatActivity {
 
+    Button buttonLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        buttonLogin = findViewById(R.id.button);
+
+        buttonLogin.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                    buttonLogin.setBackgroundResource(R.drawable.button_primary_pressed);
+                }
+                if(motionEvent.getAction() == MotionEvent.ACTION_UP){
+                    buttonLogin.setBackgroundResource(R.drawable.button_primary);
+                }
+                return false;
+            }
+        });
     }
+
+
 }
